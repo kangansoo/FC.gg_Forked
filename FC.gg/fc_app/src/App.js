71998d03e44ccf6442965cef372e407017f248f4
svng-zu <zztisdudoo@gmail.com>
2024-03-29 10:30:10 +0900

    ✨ Feat 조회 결과 페이지 디자인 구현

--- FC.gg/fc_app/src/App.js
+++ FC.gg/fc_app/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import SearchPage from './pages/SearchPage';
+import ResultPage from './pages/ResultPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<><SearchPage/></>} />
+        <Route path='/result' element={<><ResultPage/></>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ FC.gg/fc_app/src/pages/ResultPage.js
@@ -0,0 +1,66 @@
+import React, {useState} from "react"
+import "../css/Result.css"
+import gradeIcon from '../assets/ico_rank1.png' 
+import searchIcon from '../assets/searchicon.png'
+
+export default function Screen() {
+  const [searchText, setSearchText] = useState("");
+  const onChange = (e) => {
+      e.preventDefault();
+      setSearchText(e.target.value);
+  }
+  return (
+    <div className="screen">
+      <div className="div">
+
+        
+        <div className="group">
+          <div className="overlap-group">
+            <p className="text-wrapper-3">패배 상대 스코어 : 내 스코어</p>
+            <div className="text-wrapper-2">경기 날짜</div>
+          </div>
+        </div>
+        
+        <div className="overlap-group-wrapper">
+          <div className="overlap">
+            <p className="text-wrapper-3">승리 상대 스코어 : 내 스코어</p>
+            <div className="text-wrapper-2">경기 날짜</div>
+          </div>
+        </div>
+
+        
+        <div className="group-wrapper">
+              <div className="group-3">
+                <div className="text-wrapper-6">현재등급 : 월드클래스</div>
+                <div className="text-wrapper-7">최고등급 : 챌린저</div>
+              </div>
+              <img className="ico" alt="Ico" src={gradeIcon}/>
+              <div className="group-4">
+                <div className="text-wrapper-8">레벨 : 2524</div>
+                <div className="text-wrapper-9">호날두</div>
+              </div>
+
+        </div>
+        <div className="overlap-3">
+          <div className="view">
+          </div>
+        </div>
+        <div className="group-8">
+          <div className="text-wrapper-11">공식경기</div>
+          <div className="text-wrapper-12">리그친선</div>
+          <div className="text-wrapper-13">감독모드</div>
+          <div className="text-wrapper-14">클래식 1vs1</div>
+        </div>
+        
+
+            <div className="SearchContainer">
+                <input type="text" placeholder='닉네임을 입력해주세요.' value={searchText} onChange={onChange} className="SearchNickname"/>
+                <img src={searchIcon} alt="searchIcon" className='SearchIcon'/>
+            </div>
+
+    
+        
+      </div>
+    </div>
+  );
+};
