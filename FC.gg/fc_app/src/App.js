4d5a123a8ec6e25f9392df7a7229de5aab149950
kangansoo <ansoo971@gmail.com>
2024-05-24 16:34:22 +0900

    에러페이지 추가

--- FC.gg/fc_app/src/App.js
+++ FC.gg/fc_app/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import SearchPage from './pages/SearchPage';
 import ResultPage from './pages/ResultPage';
+import Test from './pages/Test';
+import ErrorPage from './pages/ErrorPage'
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path='/' element={<><SearchPage/></>} />
         <Route path='/result' element={<><ResultPage/></>} />
+        <Route path='/test' element={<><Test/></>} />
+        <Route path='/*' element={<><ErrorPage/></>} />
       </Routes>
     </BrowserRouter>
   );
